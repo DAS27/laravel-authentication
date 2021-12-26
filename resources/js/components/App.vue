6254da7da4e0783f4b7c3e9ea0969debe9e2c709
d.sarkulin <sarkulindamir@gmail.com>
2021-12-26 16:05:15 +0500

    edited vue component, added getToken input. added create token method.

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,14 +1,54 @@
 <template>
     <div class="w-full">
-        <h2 class="text-gray-900 text-2xl mb-8 font-medium title-font">Welcome</h2>
-        <div v-for="entry in entries" :id="entry.id">
-            <h3 class="text-gray-900 mb-1 font-medium title-font" v-text="entry.title"></h3>
-            <p class="text-gray-500 mb-1 font-normal" v-text="entry.content"></p>
+        <div v-if="user">
+            <h2 class="text-gray-900 text-2xl mb-8 font-medium title-font">Welcome</h2>
+            <div v-for="entry in entries" :id="entry.id">
+                <h3 class="text-gray-900 mb-1 font-medium title-font" v-text="entry.title"></h3>
+                <p class="text-gray-500 mb-1 font-normal" v-text="entry.content"></p>
+            </div>
+            <button
+                class="text-white bg-indigo-500 border-0 py-2 px-6 mr-4 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                @click="getEntries"
+            >Get My Entries</button>
+            <button
+                class="text-white bg-indigo-500 border-0 py-2 px-6 mr-4 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                @click="getToken"
+            >Get A Token</button>
+            <input
+                type="text"
+                disabled
+                class="w-full my-4 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                v-model="token"
+            >
         </div>
-        <button
-            class="text-white bg-indigo-500 border-0 py-2 px-6 mr-4 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            @click="getEntries"
-        >Get My Entries</button>
+        <div v-else>
+            <div class="form">
+                <div class="relative mb-4">
+                    <label for="email" class="leading-7 text-sm text-gray-600">Email Address</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        v-model="login.email"
+                    >
+                </div>
+                <div class="relative mb-4">
+                    <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        v-model="login.password"
+                    >
+                </div>
+                <button
+                    class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                    @click="handleLogin"
+                >Sign In</button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -16,12 +56,40 @@
         data() {
             return {
                 entries: [],
+                user: null,
+                login: {
+                    email: '',
+                    password: ''
+                },
+                token: ''
             }
         },
+        created() {
+            axios.get('/sanctum/csrf-cookie').then(response => {
+                this.getUser();
+            });
+        },
         methods: {
+            getUser() {
+                axios.get('/api/user').then(response => {
+                    this.user = response.data;
+                });
+            },
+            handleLogin() {
+                axios.post('/login', this.login).then(response => {
+                    this.getUser();
+                });
+            },
             getEntries() {
                 axios.get('/api/entries').then(response => {
                     this.entries = response.data;
+                })
+            },
+            getToken() {
+                axios.post('/api/tokens/create', {
+                    token_name: 'My Token'
+                }).then(response => {
+                    this.token = response.data.token;
                 })
             }
         }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/entries', function (Request $request) {
     return $request->user()->entries;
 })->middleware('auth:sanctum');
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+})->middleware('auth:sanctum');
